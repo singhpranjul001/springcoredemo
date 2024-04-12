3a7ceb1084f63dd7323b2f6db409115d355a33e5
Pranjul Singh <singhpranjul001@gmail.com>
2024-04-12 17:14:10 +0530

    Initial commit

---
+++ src/main/java/com/practice/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.practice.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/practice/springcoredemo/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.practice.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 30 minutes!!!!";
+    }
+}
