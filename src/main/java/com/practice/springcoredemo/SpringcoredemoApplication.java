4e95f94108cc4b1fd9402ea1e8eb5d492ad71e96
Pranjul Singh <singhpranjul001@gmail.com>
2024-04-12 18:54:56 +0530

    Learned about component scanning.

--- src/main/java/com/practice/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/practice/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,11 @@ package com.practice.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication/*(
+		//explicitly scanning the packages in com.practice package
+		scanBasePackages = {"com.practice.util",
+		 "com.practice.springcoredemo"}
+)*/
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/practice/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/practice/springcoredemo/common/CricketCoach.java
@@ -1,4 +1,4 @@
-package com.practice.springcoredemo;
+package com.practice.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 30 minutes!!!!";
+        return "Practice fast bowling for 30 minutes.";
     }
 }
